29aa132a0efbd47f549ce652dacd85ea82c8d526
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-11-29 16:21:46 +0000

    Omitting optional summary in error summary won't render empty <p> tag

--- components/gov-error-summary/test.js
+++ components/gov-error-summary/test.js
@@ -44,6 +44,36 @@ describe('<gov-error-summary/>', () => {
     );
   });
 
+  it('should render without summary <p> if no summary is supplied', () => {
+    const templateSrc =
+      `<gov-error-summary heading=data.heading summary=data.summary
+         errors=data.errors/>`;
+
+    const data = {
+      heading: 'You have errors!',
+      errors: {
+        colour: 'Pick a colour',
+        age: 'You are too old'
+      }
+    };
+
+    const output = marko.load(templatePath, templateSrc).renderSync(data);
+
+    expect(output).to.equal(
+      '<div class="error-summary" role="group" ' +
+        'aria-labelledby="error-summary-heading" tabindex="-1">' +
+        '<h1 class="heading-medium error-summary-heading" ' +
+          'id="error-summary-heading">' +
+            'You have errors!' +
+        '</h1>' +
+        '<ul class="error-summary-list">' +
+          '<li><a href="#error-message-colour">Pick a colour</a></li>' +
+          '<li><a href="#error-message-age">You are too old</a></li>' +
+        '</ul>' +
+      '</div>'
+    );
+  });
+
   it('should not render if there are no errors', () => {
     const templateSrc =
       `<gov-error-summary heading=data.heading summary=data.summary
