a8731eb4d9205998bfbdf3174cab2464cfdd1139
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2017-03-27 21:52:47 +0100

    Add empty input test for beta-banner

--- components/gov-beta-banner/test/test-server.js
+++ components/gov-beta-banner/test/test-server.js
@@ -1,5 +1,16 @@
 const assert = require('assert');
 
+test('render', context => {
+  const output = context.render({});
+  assert.equal(output.html,
+    '<div class="phase-banner-beta">' +
+      '<p>' +
+        '<strong class="phase-tag">BETA</strong>' +
+        '<span></span>' +
+      '</p>' +
+    '</div>');
+});
+
 test('render-body', context => {
   const output = context.render({renderBody: 'Please leave some feedback'});
   assert.equal(output.html,
