4d6772be924af3e08173a63c0127e81b5c7b0790
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2017-03-28 22:31:00 +0100

    Can set phase in beta-banner

--- components/gov-beta-banner/test/test-server.js
+++ components/gov-beta-banner/test/test-server.js
@@ -3,9 +3,20 @@ const assert = require('assert');
 test('render', context => {
   const output = context.render({});
   assert.equal(output.html,
-    '<div class="phase-banner-beta">' +
+    '<div class="phase-banner">' +
       '<p>' +
         '<strong class="phase-tag">BETA</strong>' +
+        '<span></span>' +
+      '</p>' +
+    '</div>');
+});
+
+test('phase', context => {
+  const output = context.render({phase: 'alpha'});
+  assert.equal(output.html,
+    '<div class="phase-banner">' +
+      '<p>' +
+        '<strong class="phase-tag">ALPHA</strong>' +
         '<span></span>' +
       '</p>' +
     '</div>');
@@ -14,7 +25,7 @@ test('render', context => {
 test('render-body', context => {
   const output = context.render({renderBody: 'Please leave some feedback'});
   assert.equal(output.html,
-    '<div class="phase-banner-beta">' +
+    '<div class="phase-banner">' +
       '<p>' +
         '<strong class="phase-tag">BETA</strong>' +
         '<span>Please leave some feedback</span>' +
